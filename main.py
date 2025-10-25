47950ee1e7eb33fde0a6a2e629ede84ac0a64479
Aryan Gautam <aryannsamraat086@gmail.com>
2025-10-25 19:23:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,6 +61,6 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
-    port = int(os.environ.get("PORT", 8000))  # Render's 
+    port = int(os.environ.get("PORT", 1000))  # Render's 
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
